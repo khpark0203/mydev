72c1265cf1fdf7982b9b3918ec5ad13413359c76
khpark0203 <khpark0203@gmail.com>
2020-05-22 09:18:15 +0900

    gitt대신에 gitm 추가 (경험상 이게 더 자주 씀..)

--- git_tool/shell/copy_tool.sh
+++ git_tool/shell/copy_tool.sh
@@ -2,4 +2,9 @@ sudo cp ./auto_complete/* /etc/bash_completion.d/
 echo "Copy to /etc/bash_completeion.d [`ls -m ./auto_complete`]"
 sudo cp ./script/* /usr/local/bin/
 echo "Copy to /usr/local/bin [`ls -m ./script`]"
+cd /usr/local/bin
+if [ ! -f gitm ]; then
+	sudo ln -s mgit gitm
+	echo "link in /usr/local/bin [mgit -> gitm]"
+fi
 sync
