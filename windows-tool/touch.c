a0abf29e9b1bf7ae77d5b69233a6d7f04f727ade
Kwan-ho <khpark0203@gmail.com>
2021-06-22 14:06:33 +0900

    touch는 생성만 되도록 함

--- windows-tool/touch.c
+++ windows-tool/touch.c
@@ -7,9 +7,7 @@ void main(int args, char* argv[])
     int i = 0;
     for (i = 1; i < args; i++) {
         char* file = argv[i];
-        if (access(file, F_OK) == 0) {
-            utime(file, NULL);
-        } else {
+        if (access(file, F_OK) != 0) {
             FILE* fp = fopen(file, "w");
             fclose(fp);
         }
