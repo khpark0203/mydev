07f7e87c154ba670109bcea2f1daac57ba076e8a
khpark0203 <khpark0203@gmail.com>
2022-06-15 08:45:45 +0900

    powershell module add(custom)

---
+++ windows-tool/gocomplete.psm1
@@ -0,0 +1,236 @@
+$goCompleter = {
+    param($wordToComplete, $commandAst, $cursorPosition)
+
+    $tokens = $commandAst.Extent.Text.Trim() -split "\s+"
+
+    $completions = switch ($tokens[1]) {
+        "bug" {
+            break
+        }
+        "build" {
+            break
+        }
+        "clean" {
+            break
+        }
+        "doc" {
+            break
+        }
+        "env" {
+            break
+        }
+        "fix" {
+            break
+        }
+        "fmt" {
+            break
+        }
+        "generate" {
+            break
+        }
+        "get" {
+            switch ($tokens[2]) {
+                "github.com/" {
+                    break
+                }
+                "golang.org/" {
+                    break
+                }
+
+                default {
+                    "github.com/"
+                    "golang.org/"
+                }
+            }
+
+            break
+        }
+        "install" {
+            switch ($tokens[2]) {
+                "github.com/" {
+                    break
+                }
+                "golang.org/" {
+                    break
+                }
+                
+                default {
+                    "github.com/"
+                    "golang.org/"
+                }
+            }
+
+            break
+        }
+        "list" {
+            break
+        }
+        "mod" {
+            switch ($tokens[2]) {
+                "download" {
+                    break
+                }
+                "edit" {
+                    break
+                }
+                "graph" {
+                    break
+                }
+                "init" {
+                    break
+                }
+                "tidy" {
+                    break
+                }
+                "vendor" {
+                    break
+                }
+                "verify" {
+                    break
+                }
+                "why" {
+                    break
+                }
+
+                default {
+                    "download"
+                    "edit"
+                    "graph"
+                    "init"
+                    "tidy"
+                    "vendor"
+                    "verify"
+                    "why"
+                }
+            }
+            break
+        }
+        "work" {
+            break
+        }
+        "run" {
+            break
+        }
+        "test" {
+            break
+        }
+        "tool" {
+            break
+        }
+        "version" {
+            break
+        }
+        "vet" {
+            break
+        }
+        "help" {
+            switch ($tokens[2]) {
+                "bug" {
+                    break
+                }
+                "build" {
+                    break
+                }
+                "clean" {
+                    break
+                }
+                "doc" {
+                    break
+                }
+                "env" {
+                    break
+                }
+                "fix" {
+                    break
+                }
+                "fmt" {
+                    break
+                }
+                "generate" {
+                    break
+                }
+                "get" {
+                    break
+                }
+                "install" {
+                    break
+                }
+                "list" {
+                    break
+                }
+                "mod" {
+                    break
+                }
+                "work" {
+                    break
+                }
+                "run" {
+                    break
+                }
+                "test" {
+                    break
+                }
+                "tool" {
+                    break
+                }
+                "version" {
+                    break
+                }
+                "vet" {
+                    break
+                }
+
+                default {
+                    "bug"
+                    "build"
+                    "clean"
+                    "doc"
+                    "env"
+                    "fix"
+                    "fmt"
+                    "generate"
+                    "get"
+                    "install"
+                    "list"
+                    "mod"
+                    "work"
+                    "run"
+                    "test"
+                    "tool"
+                    "version"
+                    "vet"
+                }
+            }
+
+            break
+        }
+        
+        default {
+            "bug"
+            "build"
+            "clean"
+            "doc"
+            "env"
+            "fix"
+            "fmt"
+            "generate"
+            "get"
+            "install"
+            "list"
+            "mod"
+            "work"
+            "run"
+            "test"
+            "tool"
+            "version"
+            "vet"
+            "help"
+        }
+    }
+
+    $completions | Where-Object {$_ -like "${wordToComplete}*"} | ForEach-Object {
+        [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+    }
+}
+
+Register-ArgumentCompleter -CommandName go -Native -ScriptBlock $goCompleter
